e4496811e1a87eecf83166567fae38f6e9b1c12a
Eron Moquete <112369066+Ronkoyy@users.noreply.github.com>
2025-09-09 15:49:00 +0800

    Event Driven Act 2

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function updateDisplay() {
-    let textbox = document.getElementById("textbox");
-    let displaytext = document.getElementById("display");
-    displaytext.textContent = textbox.value;
-}
+
+const nameInput = document.getElementById("nameInput");
+const nameOutput = document.getElementById("nameOutput");
+
+nameInput.addEventListener("input", function() {
+  nameOutput.textContent = "Hello, " + nameInput.value + "!";
+});
+
+
+const quotes = [
+  "Dream big. Start small. Act now",
+  "Opportunities don’t happen. You create them.",
+  "The harder you work for something, the greater you’ll feel when you achieve it.",
+  "Push yourself, because no one else is going to do it for you.",
+  "Don’t stop when you’re tired. Stop when you’re done."
+];
+
+const quoteBtn = document.getElementById("quoteBtn");
+const quoteOutput = document.getElementById("quoteOutput");
+
+quoteBtn.addEventListener("click", function() {
+  let randomIndex = Math.floor(Math.random() * quotes.length);
+  quoteOutput.textContent = quotes[randomIndex];
+});
